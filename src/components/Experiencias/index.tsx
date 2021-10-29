4d31c9faf17ff347a21b98b811a6ca31e799450b
snax10 <carlos_santos0702@hotmail.com>
2021-10-29 11:04:43 -0300

    finish

---
+++ src/components/Experiencias/index.tsx
@@ -0,0 +1,32 @@
+import SectionTitle from '../SectionTitle';
+import ExperienciaItem from './ExperienciaItem';
+import { Container } from './styles';
+
+function Experiencias() {
+  return (
+    <Container>
+      <SectionTitle title="01 Ano" description="de experiência" />
+
+      <section>
+        <ExperienciaItem
+          year="2021"
+          title="Dev. Front-end"
+          description="Desenvolvimento e manutenção de aplicações web, utilizando ReactJs          "
+        />
+        <ExperienciaItem
+          year="2020"
+          title="Analista de sistemas"
+          description="Analista com vivência em equipes de suporte, para atuar como técnico funcional de um sistema customizado em java, analisando e resolvendo chamados."
+        />
+        <ExperienciaItem
+          year="2020"
+          title="Dev. Estagiário"
+          description="Efetuar a migração dos conteúdos em Flash e adaptá-los em HTML, Css, JavaScript e Xml.
+          "
+        />
+      </section>
+    </Container>
+  );
+}
+
+export default Experiencias;
---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
+import picture from '../../assets/pessoa.webp';
+
+function HomeHero() {
+  return (
+    <Container data-aos="fade-up">
+      {/* <img src={picture} alt="Minha foto" /> */}
+      <div>
+        <TextContainer>
+          <h1>Olá</h1>
+          <h2>Me chamo Carlos</h2>
+        </TextContainer>
+        <InfosContainer>
+          <CodeItem data-aos="zoom-in">
+            <span className="comment">//Minha apresentação</span>
+            <span className="purple">Infos</span> {'\u007B'}
+            <div>
+              Nome: <span className="blue">Carlos,</span>
+            </div>
+            <div>
+              Sobrenome: <span className="blue">Santos</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+          <CodeItem data-aos="zoom-in">
+            <span className="purple">Cargo</span> {'\u007B'}
+            <div>
+              Função: <span className="blue">Dev Front-end,</span>
+            </div>
+            <div>
+              Empresa: <span className="blue">Grupo Parvi</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+        </InfosContainer>
+      </div>
+    </Container>
+  );
+}
+
+export default HomeHero;
