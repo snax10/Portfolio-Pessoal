af2cb5abe89bf0cd28232f82a1ee133f9cc72fae
snax10 <carlos_santos0702@hotmail.com>
2021-10-29 11:16:38 -0300

    final

--- src/components/Experiencias/index.tsx
+++ src/components/Experiencias/index.tsx
@@ -11,18 +11,19 @@ function Experiencias() {
         <ExperienciaItem
           year="2021"
           title="Dev. Front-end"
-          description="Desenvolvimento e manutenção de aplicações web, utilizando ReactJs          "
+          description="Desenvolvimento e manutenção de aplicações web, utilizando ReactJs."
         />
         <ExperienciaItem
           year="2020"
           title="Analista de sistemas"
-          description="Analista com vivência em equipes de suporte, para atuar como técnico funcional de um sistema customizado em java, analisando e resolvendo chamados."
+          description="Analista com vivência em equipes de suporte, para atuar como técnico 
+          funcional de um sistema customizado em java, analisando e resolvendo chamados."
         />
         <ExperienciaItem
           year="2020"
           title="Dev. Estagiário"
-          description="Efetuar a migração dos conteúdos em Flash e adaptá-los em HTML, Css, JavaScript e Xml.
-          "
+          description="Efetuar a migração dos conteúdos em Flash e adaptá-los em HTML, Css, 
+          JavaScript e Xml."
         />
       </section>
     </Container>
